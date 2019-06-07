9d7c4d6c3f38ea576b089eb136b92bf145c0a460
Martin <martin.gravdal@gmail.com>
2019-06-07 11:50:49 +0200

    Fixed pathing

--- Innlogging/load.js
+++ Innlogging/load.js
@@ -21,6 +21,6 @@ btn.addEventListener('click', function () {
     document.getElementById("loader").style.cssText = "opacity: 1; z-index: 1;";
     document.getElementById("login-container").style.cssText = "filter: blur(1.5px); -webkit-filter: blur(1.5px);";
     window.setTimeout(function () {
-        window.location.href = "../Forside/Forside.html";
+        window.location.href = "../index.html";
     }, 3000);
 });
